8b4559decef1648aa79a9060fa62efa2640f2ce7
Carlos Corona <ccorona@gitlab.com>
2026-03-31 09:40:30 -0700

    feat: added parsing of the raw permit data for specific criteria like zipcode

--- permit-proxy/internal/models/permit.go
+++ permit-proxy/internal/models/permit.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"time"
+)
+
 type RawPermitData struct {
 	Status          string `json:"Status"`
 	Gxlocation      string `json:"gx_location"`
@@ -13,13 +17,33 @@ type RawPermitData struct {
 	SubTypeDesc     string `json:"SUBTYPEDESCRIPTION"`
 	WorkDesc        string `json:"WORKDESCRIPTION"`
 	PermitApprovals string `json:"PERMITAPPROVALS"`
-	// will need to see the best type for the dates
 	IssueDate       string `json:"ISSUEDATE"`
 	FinalDate       string `json:"FINALDATE"`
 	DwellingUnits   string `json:"DWELLINGUNITS"`
 	PermitValuation string `json:"PERMITVALUATION"`
 	SquareFootage   string `json:"SQUAREFOOTAGE"`
 	FolderRSN       string `json:"FOLDERRSN"`
+}
+
+type Permit struct {
+	Status string `json:"status"`
+	Zipcode string `json:"zipcode"`
+	ParcelNumber string `json:"parcel_number"`
+	Applicant string `json:"applicant"`
+	OwnerName string `json:"owner_name"`
+	Contractor string `json:"contractor"`
+	FolderNumber string `json:"folder_number"`
+	FolderDesc string `json:"folder_desc"`
+	FolderName string `json:"folder_name"`
+	SubTypeDesc string `json:"subtype_desc"`
+	WorkDesc string `json:"work_desc"`
+	PermitApprovals string `json:"permit_approvals"`
+	IssueDate time.Time `json:"issue_date"`
+	FinalDate time.Time `json:"final_date"`
+	DwellingUnits int64 `json:"dwelling_units"`
+	PermitValuation float64 `json:"permit_valuation"`
+	SquareFootage int64 `json:"square_footage"`
+	FolderRSN string `json:"folder_rsn"`
 }
 
 type WrapResult struct {
