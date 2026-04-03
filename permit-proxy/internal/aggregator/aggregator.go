5689bc5e222b59a7f0192dd9c913d81073b965fb
Carlos Corona <ccorona@gitlab.com>
2026-04-02 21:33:32 -0700

    add Dockerfile to deploy on Railway

--- permit-proxy/internal/aggregator/aggregator.go
+++ permit-proxy/internal/aggregator/aggregator.go
@@ -6,21 +6,21 @@ import (
 
 type ZipCount struct {
 	Zipcode string `json:"zipcode"`
-	Count int `json:"count"`
+	Count   int    `json:"count"`
 }
 
 func ByZip(p []models.Permit) ([]ZipCount, error) {
 	z := make(map[string]int)
 	c := make([]ZipCount, 0)
-	
-	for _, i := range p{
+
+	for _, i := range p {
 		z[i.Zipcode]++
 	}
 
 	for k, v := range z {
 		c = append(c, ZipCount{
 			Zipcode: k,
-			Count: v,
+			Count:   v,
 		})
 	}
 
